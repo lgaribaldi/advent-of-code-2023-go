696d7bca30d7541e961ffaa7d8bbfeafce271ef4
lgaribaldi <lucas.garibaldi.alves@gmail.com>
2023-12-14 19:52:17 -0300

    Day 12 Adding cache WIP

--- 12b.go
+++ 12b.go
@@ -6,41 +6,61 @@ import (
 	"strings"
 )
 
-var memo map[string][]int
+var memo map[string]bool
 
 func day12b(c chan string) {
 	var total int
-	memo = make(map[string][]int)
+	memo = make(map[string]bool)
 
 	for line := range c {
 		if len(line) == 0 {
 			continue
 		}
 		preSprings, annotationsPart := splitString(line, " ")
-		preAnnotations := readLineOfNumbers(annotationsPart, ",")
+		preAnnotations := make([]int, 0, 0)
+		for _, v := range readLineOfNumbers(annotationsPart, ",") {
+			preAnnotations = append(preAnnotations, int(v))
+		}
 
-		annotations := make([]int64, 0, 0)
-		for i := 0; i < 5; i++ {
+		annotations := make([]int, 0, 0)
+		for i := 0; i < 3; i++ {
 			annotations = append(annotations, preAnnotations...)
 		}
-		springs := preSprings + "?" + preSprings + "?" + preSprings + "?" + preSprings + "?" + preSprings
+		springs := preSprings + "?" + preSprings + "?" + preSprings + "." // + "?" + preSprings + "?" + preSprings
 
-		possibilities := make([]int, 0, 0)
-		nextPossibilities := make([]int, 0, 0)
-		nextPossibilities = append(possibilities, len(springs))
+		possibilities := make([]string, 0, 0)
+		nextPossibilities := make([]string, 0, 0)
+		nextPossibilities = append(possibilities, springs)
+
+		//TODO store a second map for bigger chunks
 
 		for _, v := range annotations {
+			fmt.Println(len(nextPossibilities))
 			possibilities = nextPossibilities
-			nextPossibilities = make([]int, 0, 0)
+			nextPossibilities = make([]string, 0, 0)
 			for _, currentSprings := range possibilities {
-				nextPossibilities = append(nextPossibilities, doTheThing(v, springs[len(springs)-currentSprings:])...)
+				for i := 0; i+v < len(currentSprings); i++ {
+					// nextPossibilities = append(nextPossibilities, springs[i:i+v+1])
+
+					//for j := range next {
+					if doTheThing(v, currentSprings[i:i+v+1]) {
+						nextPossibilities = append(nextPossibilities, currentSprings[i+v+1:])
+						//next[j] += currentSprings[i+annotations[0]+1:]
+						//println("Next", next[j])
+					}
+					if currentSprings[i] == '#' {
+						break
+					}
+
+				}
+				//nextPossibilities = append(nextPossibilities, doTheThing(v, currentSprings)...)
 			}
 		}
 
 		// filter out the ones who left damaged springs
 		finalPossibilities := 0
 		for _, v := range nextPossibilities {
-			if strings.Index(springs[len(springs)-v:], "#") == -1 {
+			if strings.Index(v, "#") == -1 {
 				finalPossibilities++
 			}
 		}
@@ -53,38 +73,43 @@ func day12b(c chan string) {
 
 }
 
-func doTheThing(v int64, currentSprings string) []int {
+func doTheThing(v int, currentSprings string) bool {
 	index := fmt.Sprint(v) + currentSprings
 	if elem, ok := memo[index]; ok {
 		return elem
 	}
 
-	answer := make([]int, 0, 0)
-	var start int
-	for i := range currentSprings {
+	//answer := make([]string, 0, 0)
+	// var start int
+	/*for i := range currentSprings {
 		if currentSprings[i] != '.' {
 			start = i
 			break
 		}
-	}
-	currentSprings = currentSprings[start:]
-	for i := range currentSprings {
-		// Assigns annotation to current failed spring
-		if i > 0 && currentSprings[i-1] == '#' {
-			break
-		}
+	}*/
+	//currentSprings = currentSprings[start:]
+	//for i := range currentSprings {
+	// Assigns annotation to current failed spring
+	/*if i > 0 && currentSprings[i-1] == '#' {
+		break
+	}*/
 
-		if (currentSprings[i] == '#' || currentSprings[i] == '?') &&
-			len(currentSprings[i:]) >= int(v) && strings.Index(currentSprings[i:i+int(v)], ".") == -1 &&
-			(len(currentSprings) == i+int(v) || currentSprings[i+int(v)] != '#') {
-			// possibility for next annotation
-			if len(currentSprings) < i+int(v)+2 {
-				answer = append(answer, 0)
-			} else {
-				answer = append(answer, len(string(currentSprings[i+int(v)+1:])))
-			}
-		}
+	if (currentSprings[0] == '#' || currentSprings[0] == '?') &&
+		strings.Index(currentSprings[:int(v)], ".") == -1 &&
+		(currentSprings[int(v)] != '#') {
+		// possibility for next annotation
+		//if len(currentSprings) < int(v)+2 {
+		//answer = append(answer, "")
+		memo[index] = true
+		return true
+		/*} else {
+			println("Acho que nao cai mais aqui agora")
+			// answer = append(answer, string(currentSprings[int(v)+1:]))
+		}*/
 	}
-	memo[index] = answer
-	return answer
+	//}
+	memo[index] = false
+	return false
+
+	//return answer
 }
