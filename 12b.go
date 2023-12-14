3d1b0f3d61412870b166b9546f6957ceb02f61a2
lgaribaldi <lucas.garibaldi.alves@gmail.com>
2023-12-14 20:09:56 -0300

    Day 12 part 2 complete

--- 12b.go
+++ 12b.go
@@ -21,95 +21,58 @@ func day12b(c chan string) {
 		for _, v := range readLineOfNumbers(annotationsPart, ",") {
 			preAnnotations = append(preAnnotations, int(v))
 		}
-
+		// Multiplies input by 5
 		annotations := make([]int, 0, 0)
-		for i := 0; i < 3; i++ {
+		for i := 0; i < 5; i++ {
 			annotations = append(annotations, preAnnotations...)
 		}
-		springs := preSprings + "?" + preSprings + "?" + preSprings + "." // + "?" + preSprings + "?" + preSprings
+		springs := preSprings + "?" + preSprings + "?" + preSprings + "?" + preSprings + "?" + preSprings + "."
 
-		possibilities := make([]string, 0, 0)
-		nextPossibilities := make([]string, 0, 0)
-		nextPossibilities = append(possibilities, springs)
-
-		//TODO store a second map for bigger chunks
+		possibilities := make(map[string]int)
+		nextPossibilities := make(map[string]int)
+		nextPossibilities[springs] = 1
 
 		for _, v := range annotations {
-			fmt.Println(len(nextPossibilities))
 			possibilities = nextPossibilities
-			nextPossibilities = make([]string, 0, 0)
-			for _, currentSprings := range possibilities {
+			nextPossibilities = make(map[string]int)
+			for currentSprings, ammount := range possibilities {
+				// Cuts input into the smalles possible size
 				for i := 0; i+v < len(currentSprings); i++ {
-					// nextPossibilities = append(nextPossibilities, springs[i:i+v+1])
-
-					//for j := range next {
-					if doTheThing(v, currentSprings[i:i+v+1]) {
-						nextPossibilities = append(nextPossibilities, currentSprings[i+v+1:])
-						//next[j] += currentSprings[i+annotations[0]+1:]
-						//println("Next", next[j])
+					if checkSpringMatch(v, currentSprings[i:i+v+1]) {
+						nextPossibilities[currentSprings[i+v+1:]] += ammount
 					}
 					if currentSprings[i] == '#' {
 						break
 					}
-
 				}
-				//nextPossibilities = append(nextPossibilities, doTheThing(v, currentSprings)...)
 			}
 		}
 
 		// filter out the ones who left damaged springs
 		finalPossibilities := 0
-		for _, v := range nextPossibilities {
+		for v, ammount := range nextPossibilities {
 			if strings.Index(v, "#") == -1 {
-				finalPossibilities++
+				finalPossibilities += ammount
 			}
 		}
-		log.Printf("line: %s matches: %d", line, finalPossibilities)
 		total += finalPossibilities
 	}
-
-	log.Println(memo)
-	log.Printf("12A Total: %d", total)
+	log.Printf("12B Total: %d", total)
 
 }
 
-func doTheThing(v int, currentSprings string) bool {
+func checkSpringMatch(v int, currentSprings string) bool {
 	index := fmt.Sprint(v) + currentSprings
 	if elem, ok := memo[index]; ok {
 		return elem
 	}
 
-	//answer := make([]string, 0, 0)
-	// var start int
-	/*for i := range currentSprings {
-		if currentSprings[i] != '.' {
-			start = i
-			break
-		}
-	}*/
-	//currentSprings = currentSprings[start:]
-	//for i := range currentSprings {
-	// Assigns annotation to current failed spring
-	/*if i > 0 && currentSprings[i-1] == '#' {
-		break
-	}*/
-
 	if (currentSprings[0] == '#' || currentSprings[0] == '?') &&
 		strings.Index(currentSprings[:int(v)], ".") == -1 &&
 		(currentSprings[int(v)] != '#') {
-		// possibility for next annotation
-		//if len(currentSprings) < int(v)+2 {
-		//answer = append(answer, "")
 		memo[index] = true
 		return true
-		/*} else {
-			println("Acho que nao cai mais aqui agora")
-			// answer = append(answer, string(currentSprings[int(v)+1:]))
-		}*/
 	}
-	//}
 	memo[index] = false
 	return false
-
-	//return answer
 }
